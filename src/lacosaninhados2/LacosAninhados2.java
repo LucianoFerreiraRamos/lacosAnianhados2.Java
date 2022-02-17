3bb368b06a3bbafbaaf6898b31ebda84681be853
Luciano <97047885+LucianoFerreiraRamos@users.noreply.github.com>
2022-02-17 13:58:44 -0300

    lacosAnianhados

---
+++ src/lacosaninhados2/LacosAninhados2.java
@@ -0,0 +1,15 @@
+package lacosaninhados2;
+
+public class LacosAninhados2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=6; i+=2) {
+			for(int j=0; j<=2; j++) {
+				System.out.println(i + "," +j);
+			}
+		}
+
+	}
+
+}
